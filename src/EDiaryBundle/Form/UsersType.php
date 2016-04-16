9cbfefe2febc07ae71a8da8e8ed44913e0005e18
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2016-04-17 00:59:44 +0300

    deleted rubish files, edited all views of the users

--- src/EDiaryBundle/Form/UsersType.php
+++ src/EDiaryBundle/Form/UsersType.php
@@ -5,6 +5,9 @@ namespace EDiaryBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class UsersType extends AbstractType
 {
@@ -15,10 +18,30 @@ class UsersType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('password')
-            ->add('childUsername')
-            ->add('role')
+            ->add('username', TextType::class, [
+                'label' => 'Vartotojo vardas',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Slaptažodis',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            // ->add('childUsername', EntityType::class, [
+            //     'class' => 'EDiaryBundle:Users',
+            //     'choice_label' => 'username'
+            // ])
+            ->add('role', EntityType::class, [
+                'class' => 'EDiaryBundle:Roles',
+                'choice_label' => 'title',
+                'label' => 'Rolė',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
         ;
     }
     
