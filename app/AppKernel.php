5a0d81707706d82de27f23b2ed6a6c8b15ff6f30
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2016-04-19 04:16:04 +0300

    added assetic

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new EDiaryBundle\EDiaryBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
